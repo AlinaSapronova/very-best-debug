7cd18b6ccb8519551ddea9c5e3bfbbf58d77ac83
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-10-03 18:03:03 +0000

    finished

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.new
-    comment.author_id = params.fetch("query_author_id")
-    comment.venue_id = params.fetch("query_venue_id")
-    comment.body = params.fetch("query_body")
+    @comment = Comment.new
+    @comment.author_id = params.fetch("query_author_id")
+    @comment.venue_id = params.fetch("query_venue_id")
+    @comment.body = params.fetch("query_body")
     if @comment.valid?
       @comment.save
       redirect_to("/venues/#{@comment.venue_id}", { :notice => "Comment posted successfully." })
